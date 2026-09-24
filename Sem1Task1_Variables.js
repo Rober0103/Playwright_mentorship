2df75a794c50fcdd7fb67c48d34ac1e220006f8d
Rober0103 <110065781+Rober0103@users.noreply.github.com>
2026-09-24 16:54:37 -0600

    first commit

---
+++ Sem1Task1_Variables.js
@@ -0,0 +1,16 @@
+let names= 'Rober';                                     
+let imLive= true;                                       
+let myage= 36;
+let myAddress= ['Guadalajara', 'Jalisco','Mexico']
+let today = new Date().toISOString()
+let object={
+    CompleteName:'Roberto Carlos'
+};
+
+
+console.log(names);
+console.log(imLive);
+console.log(myage);
+console.log(myAddress);
+console.log(today);
+console.log(object);
